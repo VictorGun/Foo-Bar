47b1d04db7013151bba5999aaaa7f7ee3c973367
VictorGun <Thunderboomboom7@gmail.com>
2022-08-26 16:46:00 -0500

    Initial Commit

---
+++ Foo Bar/Foo_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foo_BarApp.swift
+//  Foo Bar
+//
+//  Created by Victor Gunderson on 8/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Foo_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
